cf67b088c116107e2c025df8da1136ffde19041d
Anmolghub <anmolchaurasia125@gmail.com>
2026-05-01 13:46:09 +0530

    fix: deployment issues

--- team-task/config/db.js
+++ team-task/config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     console.log("MongoDB Connected");
   } catch (error) {
     console.error(error);
-    process.exit(1);
+    console.error("MongoDB connection failed:", error.message);
   }
 };
 
